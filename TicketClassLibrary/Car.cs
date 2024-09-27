c78f6c5ec49c3e92084a71e2c82cdb954c8311f8
soh003 <149380282+soh003@users.noreply.github.com>
2024-09-27 13:44:46 +0200

    Task 2: Class Car Methods Unitest af price and vehicle

---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    public class Car
+    {
+        //instansfelter
+        private string _licensePlate;
+        private DateTime _date;
+
+        //properties
+        public string Licenseplate
+        {
+            get { return _licensePlate; }
+            set { _licensePlate = value; }  
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        //Constructor
+        /// <summary>
+        /// Constructor til instancer af car med properties LicentePlate og date
+        /// </summary>
+        /// <param name="licenseplate"></param>
+        /// <param name="date"></param>
+        public Car(string licensePlate, DateTime date)
+        {
+            _licensePlate = licensePlate;
+            _date = date;
+        }
+
+        // Methoder
+        /// <summary>
+        /// Prismetode for car.price
+        /// </summary>
+        /// <returns> 240 </returns>
+        public double Price()
+        {
+            return 240.0;
+        }
+
+        /// <summary>
+        /// VehicleType metode for car.VehicleType
+        /// </summary>
+        /// <returns> Car </returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+
+
+
+
+
+    }
+}
---
+++ TicketClassLibraryTests/CarTests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car; 
+
+        [TestInitialize]
+        public void Init()
+        {
+            //Arrange
+            car = new Car("ABC123", DateTime.Now);
+        }
+
+
+        [TestMethod]
+
+        public void PriceTest_Ok()
+        {
+
+            // Act
+            double price = car.Price();
+
+            // Assert
+            Assert.AreEqual(240.0, price);
+        }
+
+
+        [TestMethod]
+        [DataRow(239.0)]
+        [DataRow(241.0)]
+        public void PriceTest_NotOk(double test_Pris)
+        {
+
+            // Act
+            double price = car.Price();
+
+            //Assert
+
+            Assert.AreNotEqual(test_Pris, price);
+
+
+        }
+
+        [TestMethod]
+        public void VehicleType_Ok()
+        {
+            //Arrange
+
+            //Act
+
+            string vehicleType = car.VehicleType();
+
+            //Assert
+
+            Assert.AreEqual("Car", vehicleType);
+        }
+
+        [TestMethod]
+        public void VehicleType_NotOk()
+        {
+            //Arrange
+
+            //Act
+
+            string vehicleType = car.VehicleType();
+
+            //Assert
+
+            Assert.AreNotEqual("Truck", vehicleType);
+        }
+
+
+
+
+
+    }
+}

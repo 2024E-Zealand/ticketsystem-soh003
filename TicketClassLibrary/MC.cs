d4a809c457482242adc47d257475b373e28c4817
soh003 <149380282+soh003@users.noreply.github.com>
2024-09-30 20:48:12 +0200

    Task 3 Class MC Mctest XML dokumentation

---
+++ TicketClassLibrary/MC.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    public class MC
+    {
+
+        //instansfelter
+        private string _licensePlate;
+        private DateTime _date;
+
+        //properties
+        public string Licenseplate
+        {
+            get { return _licensePlate; }
+            set { _licensePlate = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+
+        /// <summary>
+        /// Constructor for MC
+        /// </summary>
+        /// <param name="licensePlate"></param>
+        /// <param name="date"></param>
+        public MC(string licensePlate, DateTime date)
+        {
+            _licensePlate = licensePlate;
+            _date = date;
+        }
+
+
+
+
+        //Metoder
+
+        /// <summary>
+        /// Prismetode for MC.price
+        /// </summary>
+        /// <returns>125</returns>
+        public double Price()
+        {
+            return 125.0;
+        }
+
+
+        /// <summary>
+        /// Returns vehicletype string
+        /// </summary>
+        /// <returns>"MC"</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+
+    }
+}
---
+++ TicketClassLibraryTests/MCTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        private MC mc;
+
+
+        /// <summary>
+        /// Initialiserer et MC-objekt før hver testmetode.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            //Arrange
+            mc = new MC("ABC123", DateTime.Now);
+        }
+
+
+
+        /// <summary>
+        /// Tester at prismetoden returnerer det forventede
+        /// </summary>
+        [TestMethod]
+
+        public void PriceTest_Ok()
+        {
+
+            // Act
+            double price = mc.Price();
+
+            // Assert
+            Assert.AreEqual(125.0, price);
+        }
+
+        /// <summary>
+        /// Tester at prismetoden kun kan sættes til 125
+        /// </summary>
+        /// <param name="test_Pris"></param>
+        [TestMethod]
+        [DataRow(124.0)]
+        [DataRow(126.0)]
+        public void PriceTest_NotOk(double test_Pris)
+        {
+
+            // Act
+            double price = mc.Price();
+
+            //Assert
+
+            Assert.AreNotEqual(test_Pris, price);
+
+
+        }
+
+        /// <summary>
+        /// Tester at Vehivletype sættes til MC
+        /// </summary>
+        [TestMethod]
+        public void VehicleType_Ok()
+        {
+            //Arrange
+
+            //Act
+
+            string vehicleType = mc.VehicleType();
+
+            //Assert
+
+            Assert.AreEqual("MC", vehicleType);
+        }
+
+        /// <summary>
+        /// tester at type kun kan være MC
+        /// </summary>
+        [TestMethod]
+        public void VehicleType_NotOk()
+        {
+            //Arrange
+
+            //Act
+
+            string vehicleType = mc.VehicleType();
+
+            //Assert
+
+            Assert.AreNotEqual("Car", vehicleType);
+        }
+    }
+}

d72f2734722f9051a4fa0277c459edcfe276b1b3
soh003 <149380282+soh003@users.noreply.github.com>
2024-10-01 12:51:39 +0200

    Task 9 prismetode weekend rabat

---
+++ StoreBaeltTicketLibrary/StoreBaelt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketClassLibrary;
+
+namespace StoreBaeltTicketLibrary
+{
+    internal class StoreBaelt
+    {
+        public double StoreBaeltPrice(Vehicle v)
+        {
+            
+            double finalPrice = v.Price();
+
+            // Apply the weekend discount for Cars only
+            if (v.VehicleType() == "Car" && (v.Date.DayOfWeek == DayOfWeek.Saturday || v.Date.DayOfWeek == DayOfWeek.Sunday))
+            {
+                finalPrice *= 0.8; // 20% weekend discount
+            }
+
+            // Apply Brobizz discount after the weekend discount
+            if (v.Brobizz)
+            {
+                finalPrice *= 0.95; //  5% Brobizz discount
+            }
+
+            return finalPrice;
+        }
+    }
+}
